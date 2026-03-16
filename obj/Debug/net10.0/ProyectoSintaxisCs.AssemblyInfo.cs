8c796428da6349c8a631888c7a57f4f4c4ade09a
iTTzBASTIAN <navarrolopezsebastian125@gmail.com>
2026-03-16 15:59:38 -0500

    menú principal finalizado

--- obj/Debug/net10.0/ProyectoSintaxisCs.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProyectoSintaxisCs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoSintaxisCs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5246eef8bdd33b5d34329ac3ce79159374a32768")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1894290ea9468eb59dc9dd35e45fce61e3d77e54")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoSintaxisCs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoSintaxisCs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
