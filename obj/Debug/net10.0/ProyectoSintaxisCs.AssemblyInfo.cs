b09dabdaa0199c459edf0b0b9e35278ed5f3a18a
iTTzBASTIAN <navarrolopezsebastian125@gmail.com>
2026-03-18 18:22:51 -0500

    listo para hacer commit

--- obj/Debug/net10.0/ProyectoSintaxisCs.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProyectoSintaxisCs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoSintaxisCs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a70eb632462c069efd20db00c22348af8bfe519a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95b550d65d7d3e8dd0e61d621605fbc8262d688b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoSintaxisCs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoSintaxisCs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
