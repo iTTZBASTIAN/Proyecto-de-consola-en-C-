78f0e2315bd49ea60c41c5313681760076753db8
iTTzBASTIAN <navarrolopezsebastian125@gmail.com>
2026-04-06 20:04:43 -0500

    pull realizado

--- obj/Debug/net10.0/ProyectoSintaxisCs.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProyectoSintaxisCs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoSintaxisCs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49fb1b24d6d5ea1a89c8a88b3f6b27486d223b28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f31f04eb3f103add250715e4de0b97257e239723")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoSintaxisCs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoSintaxisCs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
