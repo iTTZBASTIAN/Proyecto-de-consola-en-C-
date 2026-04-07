5139cbd366f943eb8205ef404c931833139626ac
iTTzBASTIAN <navarrolopezsebastian125@gmail.com>
2026-04-07 01:59:06 -0500

    todos los cambios realizados

--- services/LibroService.cs
+++ services/LibroService.cs
@@ -5,7 +5,12 @@ namespace Books.Services
 {
     public class LibroService
     {
-        private List<Libro> libros = new List<Libro>();
+        private List<Libro> libros = new List<Libro>
+        {
+            new Libro("cien años de soledad", "Gabriel Garcia Marquez", "Fantasia", "1967", "20"),
+            new Libro("el hobbit", "J. R. R. Tolkien", "Fantasia", "1937", "15"),
+            new Libro("harry potter", "J. K. Rowling", "Fantasia", "1997", "40")
+        };
         public List<Libro> ObtenerTodos() => libros;
         public void Agregar(Libro libro) => libros.Add(libro);
         public Libro ObtenerPorIndice(int indice) 
--- services/UsuarioService.cs
+++ services/UsuarioService.cs
@@ -5,7 +5,12 @@ namespace Books.Services
 {
     public class UsuarioService
     {
-        private List<Usuario> usuarios = new List<Usuario>();
+        private List<Usuario> usuarios = new List<Usuario>
+        {
+            new Usuario("Jose", "305-457820", true, "2"),
+            new Usuario("Joseluis", "314-34566", true, "0"),
+            new Usuario("Karim", "315-467752", true, "3")
+        };
 
         public List<Usuario> ObtenerTodos() => usuarios;
 
