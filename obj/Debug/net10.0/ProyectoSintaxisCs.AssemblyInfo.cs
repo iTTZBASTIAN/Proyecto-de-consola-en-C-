b79de2353b919831894e0335f5b0a5bf76e41091
iTTzBASTIAN <navarrolopezsebastian125@gmail.com>
2026-04-07 00:22:48 -0500

    archivo creado

--- obj/Debug/net10.0/ProyectoSintaxisCs.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProyectoSintaxisCs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoSintaxisCs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+762f2d6b91c22c3b0252a63e8c55cb41f26fb928")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc9f6b967a0f9e78d0fd9ea621e717615d855f36")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoSintaxisCs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoSintaxisCs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
